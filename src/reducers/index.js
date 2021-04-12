cf01d1b085ae844fb0430914f00d68d1b79ab4c3
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-12 11:09:27 -0300

    Combined reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => [
   {
     title: 'Speak to Me',
@@ -47,3 +49,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
   }
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
