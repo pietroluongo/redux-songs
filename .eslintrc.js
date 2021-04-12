28d3edcc04ed51a116b57d5410bf977b40520870
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-12 10:53:57 -0300

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'jsx-a11y/label-has-associated-control': ['error', {
+      required: {
+        some: ['nesting', 'id'],
+      },
+    }],
+    'react/prefer-stateless-function': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'max-len': 'off',
+    'no-console': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': [0, { allow: 'literal' }],
+    'jsx-a11y/interactive-supports-focus': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+
+  },
+};
